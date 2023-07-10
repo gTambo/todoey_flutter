21999ee6dbe6273d1d547edbbc46dc652f121daa
Greg Tambornino <greg.tambornino@concordusa.com>
2023-07-10 17:37:31 -0500

    tasks screen initial setup

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding:
+            const EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+        child: const Column(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 30,
+              child: Icon(
+                Icons.list,
+                size: 30,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
