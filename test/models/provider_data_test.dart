a1d4cb18d0064ed745b54bb3e9e5d556a920ffd2
Greg Tambornino <greg.tambornino@concordusa.com>
2023-08-16 17:10:23 -0500

    taskdata test checking for notifyListeners

--- test/models/provider_data_test.dart
+++ test/models/provider_data_test.dart
@@ -1,11 +1,39 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:collection';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:todoey_flutter/models/provider_data.dart';
 import 'package:todoey_flutter/models/task.dart';
 
+class MockCallbackFunction extends Mock {
+  call();
+}
+
 void main() {
-  group('Testing Provider', () {
-    final List<Task> tasks = [];
-    test('description', () => null);
+  group('$TaskData', () {
+    late TaskData taskData;
+    final notifyListenerCallback =
+        MockCallbackFunction(); // Your callback function mock
+
+    setUp(() {
+      taskData = TaskData()..addListener(notifyListenerCallback);
+      reset(notifyListenerCallback); // resets your mock before each test
+    });
+
+    test('adds a task and calls listeners', () {
+      taskData.addTask('task one');
+      expect(taskData.taskCount, 1);
+      taskData.addTask('task two');
+      verify(notifyListenerCallback())
+          .called(2); // verify listener were notified twice
+    });
+
+    test('unit tests are independent from each other', () {
+      taskData.addTask('task one');
+      expect(taskData.taskCount, 1);
+      taskData.addTask('task two');
+      verify(notifyListenerCallback()).called(
+          2); // verify listener were notified twice. This only works, if you have reset your mocks
+    });
   });
 }
