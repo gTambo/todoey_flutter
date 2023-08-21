7179c2d87541982951308480a80a8979b766c304
Greg Tambornino <greg.tambornino@concordusa.com>
2023-08-21 16:54:06 -0500

    add task button test

--- test/screens/add_task_test.dart
+++ test/screens/add_task_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,6 +25,16 @@ void main() {
       await widgetTester.pumpWidget(createAddTaskScreen());
       await widgetTester.pumpAndSettle();
       expect(find.byType(TextField), findsOneWidget);
+    });
+    testWidgets('"Add" button dismisses widget', (widgetTester) async {
+      await widgetTester.pumpWidget(createAddTaskScreen());
+      await widgetTester.pumpAndSettle();
+      var addButton = find.byType(TextButton).first;
+      expect(addButton, findsOneWidget);
+      await widgetTester.enterText(find.byType(TextField), 'test task');
+      await widgetTester.tap(addButton);
+      await widgetTester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.byType(TextButton), findsNothing);
     });
   });
 }
